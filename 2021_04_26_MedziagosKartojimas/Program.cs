8ccfc6db83101d27329dcfdccecfec5fb7f26aa7
Linas <66268230+linas92@users.noreply.github.com>
2021-04-26 20:13:51 +0300

    Add project files.

---
+++ 2021_04_26_MedziagosKartojimas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2021_04_26_MedziagosKartojimas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
