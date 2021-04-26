624a41675ed4df8a3be7dbea45c94b7c0e4cdf33
Linas <66268230+linas92@users.noreply.github.com>
2021-04-26 21:21:33 +0300

    asdrfsdgsdfg

---
+++ 2021_04_26_MedziagosKartojimas/BKlase.cs
@@ -0,0 +1,24 @@
+﻿namespace _2021_04_26_MedziagosKartojimas
+{
+    public class BKlase : BaseClass
+    {
+        public BKlase(string name) : base(name)
+        {
+        }
+
+        public override int NameKodas(int WHATTHEFUCK)
+        {
+            int suma = 0;
+            foreach (var raide in Name)
+            {
+                suma -= (int)raide * WHATTHEFUCK;
+            }
+            return suma;
+        }
+
+        public override string NameMetodas()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}//namespace _2021_04_26_MedziagosKartojimas
